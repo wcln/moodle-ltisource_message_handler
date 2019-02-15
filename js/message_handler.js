31ab5190424fe3273e9625d0ac2ea42120fe9166
colinbernard <colinjbernard@hotmail.com>
2019-02-15 14:54:48 -0800

    Added license block and function comments to JS file

--- js/message_handler.js
+++ js/message_handler.js
@@ -1,9 +1,36 @@
 
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Injects two functions into consumer site which can receive messages from the consumer.
+ *
+ * @package    ltisource_message_handler
+ * @copyright  2019 Colin Bernard {@link https://wcln.ca}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 var messageHandlerScript = window.parent.document.createElement('script');
 messageHandlerScript.type = 'text/javascript';
 messageHandlerScript.innerHTML = 'monitorLtiMessages();' + ltiMessageHandler.toString() + monitorLtiMessages.toString();
 window.parent.document.body.appendChild(messageHandlerScript);
 
+/**
+ * Handles incoming messages.
+ * @param  {[object]} e A message event.
+ */
 function ltiMessageHandler(e) {
 
   try {
@@ -35,6 +62,9 @@ function ltiMessageHandler(e) {
   }
 }
 
+/**
+ * Adds an event listener to the window to listen for LTI messages.
+ */
 function monitorLtiMessages() {
   window.addEventListener('message', function(e) {
     ltiMessageHandler(e);
