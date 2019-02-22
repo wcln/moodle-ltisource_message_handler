4c6a6efb91f299fcee4975bb78fc0e1518207cc0
colinbernard <colinjbernard@hotmail.com>
2019-02-22 13:25:59 -0800

    Checking if moodle internal

--- lib.php
+++ lib.php
@@ -22,6 +22,7 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+defined('MOODLE_INTERNAL') || die();
 
 /**
  * Implementation of before_launch callback. (https://docs.moodle.org/dev/Callbacks)
--- version.php
+++ version.php
@@ -23,6 +23,7 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
+
 $plugin->version   = 2019022200; // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2017111300; // Requires this Moodle version.
 $plugin->component = 'ltisource_message_handler'; // Full name of the plugin (used for diagnostics).
